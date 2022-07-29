05d2e730eaf6186b651bb5231560e2b30acabd4a
Shivam Maurya <mauryashivam105@gmail.com>
2022-07-29 13:22:01 +0530

    Day 5 completed

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:first_demo_apk/utils/routes.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -7,50 +8,55 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Center(
+      child: SingleChildScrollView(
         child: Column(
           children: [
-            Image.asset(
-              'assets/images/login_page.jpg',
-              fit: BoxFit.cover,
-            ),
-            /*SizedBox(
-              height: 20.0,
-              child: Text("Hello"),
-            ),*/
-            Text(
-              "Login",
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                        hintText: "Enter Username", labelText: "Username"),
+            Column(
+              children: [
+                Image.asset(
+                  'assets/images/login_page.jpg',
+                  fit: BoxFit.cover,
+                ),
+                /*SizedBox(
+                  height: 20.0,
+                  child: Text("Hello"),
+                ),*/
+                Text(
+                  "Login",
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 16.0, horizontal: 32.0),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                            hintText: "Enter Username", labelText: "Username"),
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            hintText: "Enter Password", labelText: "Password"),
+                      ),
+                      SizedBox(
+                        height: 40.0,
+                      ),
+                      ElevatedButton(
+                        child: Text("Login"),
+                        style: TextButton.styleFrom(minimumSize: Size(120, 50)),
+                        onPressed: () {
+                          Navigator.pushNamed(context, MyRoutes.homeRoute);
+                        },
+                      )
+                    ],
                   ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        hintText: "Enter Password", labelText: "Password"),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  ElevatedButton(
-                    child: Text("Login"),
-                    onPressed: () {
-                      print("Logged In")
-                    },
-                  )
-                ],
-              ),
-            )
+                )
+              ],
+            ),
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:first_demo_apk/Pages/home_page.dart';
 import 'package:first_demo_apk/Pages/login_page.dart';
+import 'package:first_demo_apk/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -14,14 +15,18 @@ class Myapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.dark,
-      theme: ThemeData(primarySwatch: Colors.red,
-      fontFamily: GoogleFonts.lato().fontFamily
-      ),
+      theme: ThemeData(
+          primarySwatch: Colors.red, fontFamily: GoogleFonts.lato().fontFamily),
+      //debugShowCheckedModeBanner: false,
       darkTheme: ThemeData(
         brightness: Brightness.light,
         primarySwatch: Colors.purple,
       ),
-      routes: {"/": (context) => LoginPage(), "/home": (context) => Homepage()},
+      routes: {
+        "/": (context) => LoginPage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => Homepage()
+      },
     );
   }
 }
