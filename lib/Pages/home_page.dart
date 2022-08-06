8b4cf353a3e4ddcecea1f5b8fd86f3edb6efe93b
Shivam Maurya <mauryashivam105@gmail.com>
2022-08-06 09:31:47 +0530

    Day 13 Completed

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,33 +1,59 @@
 import 'package:first_demo_apk/models/catolog.dart';
 import 'package:first_demo_apk/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import '../widgets/drawer.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   Homepage({Key? key}) : super(key: key);
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
   int days = 30;
   String name = "Maurya";
   @override
-  Widget build(BuildContext context) {
-    final dummylist = List.generate(50, ((index) => CatalogModel.items[0]));
+  void initState() {
+    super.initState();
+    loadData();
+  }
 
+  loadData() async {
+    await Future.delayed(const Duration(seconds: 2));
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Login Page"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: dummylist.length,
+        child: (CatalogModel.items!=null&&CatalogModel.items.isNotEmpty)? ListView.builder(
+          itemCount: CatalogModel.items.length,
           itemBuilder: ((context, index) {
             return ItemWidget(
-              item: dummylist[index],
+              item: CatalogModel.items[index],
             );
           }),
+        )
+        :const Center(
+          child: CircularProgressIndicator(),
         ),
       ),
-      drawer: MyDrawer(),
+      drawer: const MyDrawer(),
     );
   }
 }
