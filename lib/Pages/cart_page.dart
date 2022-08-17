6955df1ad5971449b8db810d61d601e7b06f4680
Shivam Maurya <mauryashivam105@gmail.com>
2022-08-17 11:24:00 +0530

    Day 21 copleted

--- lib/Pages/cart_page.dart
+++ lib/Pages/cart_page.dart
@@ -32,7 +32,10 @@ class _CartTotal extends StatelessWidget {
           "\$9999".text.xl4.color(context.theme.accentColor).make(),
           30.widthBox,
           ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: "Buying Not Supported yet".text.make()));
+                  },
                   style: ButtonStyle(
                       backgroundColor:
                           MaterialStateProperty.all(context.theme.buttonColor)),
